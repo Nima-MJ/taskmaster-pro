a7bd9a97440e3901b4c5d04ad5d6069d81ad2712
Nima Mehrjoo <nmehrjoo1999@gmail.com>
2022-01-21 16:27:21 -0500

    date upcoming and past

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +15,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -124,29 +128,33 @@ $(".list-group").on("blur", "textarea", function(){
   $(this).replaceWith(taskP);
 });
 
-//due date was clicked 
-$(".list-group").on("click", "span", function(){
-  //get current text
-  var date = $(this)
-    .text()
-    .trim();
+//edit task date picker
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this).text().trim();
 
-  //create a new input element
-  var dateInput = $("<input>")
-    .attr("type", "text") // .attr with one arguement gets an attribute (like id) but with two arguments it sets attribute (type = "text")
-    .addClass("form-control")
-    .val(date);
+  // create new input element
+  var dateInput = $("<input>").attr("type", "text").addClass("form-control").val(date);
 
-  //swap out elements
   $(this).replaceWith(dateInput);
 
-  //automatically focus on the new element 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onclose: function(){
+      // when calender is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
+
+  // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 
+
 //value of due date was changed 
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
   //get current text
   var date = $(this)
     .val()
@@ -173,6 +181,9 @@ $(".list-group").on("blur", "input[type='text']", function(){
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //[ass task's <Li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -249,6 +260,34 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+// date picker for tasks dates in the modal
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+//audit the tasks if within 2 days yellow, if due date in the past red
+var auditTask = function(taskEl){
+  //get date from task element 
+  var date = $(taskEl).find("span").text().trim();
+  console.log(date);
+
+  //convert to moment object at 5:00pm
+  var time = moment(date,"L").set("hour",17);
+  console.log(time);
+
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  //apply new class if task is near/over due date
+  if(moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if(Math.abs(moment().diff(time, "days")) <= 2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+}
+
 // load tasks for the first time
 loadTasks();
 
